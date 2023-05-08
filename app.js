f6aa842e734daa3958a97cd45a9c45a33988d2c6
mitchellkealy <kealym@oregonstate.edu>
2023-05-08 15:32:14 -0500

    Changed to JSON response for easier manipulation. Changed URL to BMI. BMI now returns as an integer.

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const app = express();
 
-const convert = require('json-to-plain-text');
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.post("/url", bodyParser.json(), (req, res) => {
+app.post("/bmi", bodyParser.json(), (req, res) => {
     const body = req.body
     const height = body.height
-    body.bmi = (body.weight / (height * height) * 703).toFixed(1)
-    const text = convert.toPlainText(body)
-    res.attachment('filename.txt')
-    res.type('txt')
-    res.send(text)
+    body.bmi = parseInt((body.weight / (height * height) * 703).toFixed(1))
+    res.type('json')
+    res.send(body)
 });
 
 app.listen(3000, () => {
